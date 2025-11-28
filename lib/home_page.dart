34ed0beea2264b30d7d5c2687ae6f4a8460343a5
khoirunnisasaputri589-crypto <khoirunnisasaputri589@gmail.com>
2025-11-28 13:19:06 +0700

    update

---
+++ lib/home_page.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.transparent),
+      body: Column(
+        children: [
+          Transform.translate(
+            offset: Offset(0, -50),
+            child: Container(
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.brown[200],
+                image: DecorationImage(
+                  image: AssetImage("assets/images/splash.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsetsGeometry.symmetric(horizontal: 30),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Search",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+          ),
+
+          //produk2
+          Expanded(
+            child: Padding(
+              padding: EdgeInsetsGeometry.symmetric(horizontal: 30),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Image(
+                          height: 130,
+                          image: AssetImage("assets/images/splash.png"),
+                        ),
+                        SizedBox(width: 20),
+                        Expanded(child: Text("Kopi Latte")),
+                        Column(children: [Icon(Icons.star), Text("5")]),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        Image(
+                          height: 130,
+                          image: AssetImage("assets/images/splash.png"),
+                        ),
+                        SizedBox(width: 20),
+                        Expanded(child: Text("Kopi Latte")),
+                        Icon(Icons.star),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        Image(
+                          height: 130,
+                          image: AssetImage("assets/images/splash.png"),
+                        ),
+                        SizedBox(width: 20),
+                        Expanded(child: Text("Kopi Latte")),
+                        Icon(Icons.star),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        Image(
+                          height: 130,
+                          image: AssetImage("assets/images/splash.png"),
+                        ),
+                        SizedBox(width: 20),
+                        Expanded(child: Text("Kopi Latte")),
+                        Icon(Icons.star),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        Image(
+                          height: 130,
+                          image: AssetImage("assets/images/splash.png"),
+                        ),
+                        SizedBox(width: 20),
+                        Expanded(child: Text("Kopi Latte")),
+                        Icon(Icons.star),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(
+        255,
+        203,
+        119,
+        88,
+      ), // background coklat
+      body: Center(
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Image(image: AssetImage("assets/images/splash.png")),
+            Text(
+              "Kopie",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
